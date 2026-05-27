8c1feee8d86c8e6ddb1387d47769b02e43bb31a9
digitallads <153294085+digitallads@users.noreply.github.com>
2026-05-27 19:57:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = Fastify({
 
 const connection = new IORedis({
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+  port: process.env.REDIS_PORT,
   maxRetriesPerRequest: null
 });
 
